918700ec5f85c4617a8d9a73271fea71ae57982d
Dharanish kumar Mallela <M1016931@mindtree.com>
2018-10-18 03:23:06 +0530

    first commit

---
+++ DesignPatterns/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using DesignPatterns.Singleton_Pattern;
+
+namespace DesignPatterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestClass.Name = "TestName";
+            TestClass.Name = "Testname1";
+        }
+    }
+
+    public class A
+    {
+        public int value1 = 10;
+    }
+
+    public class b
+    {
+        public int value1 = 10;
+    }
+}
---
+++ DesignPatterns/Singleton Pattern/Singleton.cs
@@ -0,0 +1,44 @@
+﻿namespace DesignPatterns.Singleton_Pattern
+{
+    public static class SingletonGeneric<T> where T : new()
+
+    {
+        private static T _singletonObject;
+
+        public static T SingleInstance
+        {
+            get
+            {
+                if (_singletonObject == null)
+                {
+                    _singletonObject = new T();
+                }
+
+                return _singletonObject;
+            }
+        }
+    }
+
+    //Use static methods in instance classes only when those methods are not dependent on instance variables.
+    //If they are dependent on instance variables then use instance methods.
+
+    public sealed class SingletonEagar
+    {
+        private static SingletonEagar singletonEagar = new SingletonEagar();
+
+        private SingletonEagar()
+        {
+
+        }
+
+        public SingletonEagar GetInstance()
+        {
+            return singletonEagar;
+        }
+    }
+
+    public class TestClass
+    {
+        public static string Name;
+    }
+}
